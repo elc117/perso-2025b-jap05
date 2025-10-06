4e19a45d31222a965dc1b407fc1d9b8a53090ddf
José Arthur Perius <josearthurperius@gmail.com>
2025-10-06 01:30:02 -0300

    Testes das funções

---
+++ Testes.hs
@@ -0,0 +1,76 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Aeson
+import System.Random (randomRIO)
+
+data Tanque = Tanque
+  { nome    :: String
+  , nacao   :: String
+  , calibre :: String
+  } deriving (Show)
+
+instance ToJSON Tanque where
+  toJSON (Tanque nome' nacao' calibre') =
+    object [ "nome"    .= nome'
+           , "nação"   .= nacao'
+           , "calibre" .= calibre'
+           ]
+  
+  toEncoding (Tanque nome' nacao' calibre') =
+    pairs ( "nome"    .= nome'
+         <> "nação"   .= nacao'
+         <> "calibre" .= calibre'
+          )
+
+baseDeTanques :: [Tanque]
+baseDeTanques =
+  [ Tanque "Tiger I" "Alemanha" "88mm"
+  , Tanque "T-34" "União Soviética" "76.2mm"
+  , Tanque "M4 Sherman" "Estados Unidos" "75mm"
+  , Tanque "Churchill" "Reino Unido" "57mm"
+  , Tanque "Panzer IV" "Alemanha" "75mm"
+  , Tanque "IS-2" "União Soviética" "122mm"
+  , Tanque "Panther A" "Alemanha" "88mm"
+  , Tanque "SU-100" "União Soviética" "100mm"
+  ]
+
+getElementoAleatorio :: [a] -> IO a
+getElementoAleatorio xs = do
+  indice <- randomRIO (0, length xs - 1)
+  return $ xs !! indice
+
+getTanqueAleatorio :: IO Tanque
+getTanqueAleatorio = getElementoAleatorio baseDeTanques
+
+main :: IO ()
+main = do
+  putStrLn "=== TESTES DO SISTEMA DE TANQUES ==="
+  putStrLn ""
+  
+  putStrLn "Teste 1: Verificar quantidade de tanques"
+  putStrLn $ "Total de tanques na base: " ++ show (length baseDeTanques)
+  putStrLn "Esperado: 8"
+  putStrLn $ "Resultado: " ++ if length baseDeTanques == 8 then "PASSOU" else "FALHOU"
+  putStrLn ""
+
+  putStrLn "Teste 2: Listar todos os tanques"
+  mapM_ (\t -> putStrLn $ "  - " ++ nome t ++ " (" ++ nacao t ++ ") - " ++ calibre t) baseDeTanques
+  putStrLn "Resultado: PASSOU"
+  putStrLn ""
+   
+  putStrLn "Teste 5: Testar getTanqueAleatorio"
+  mapM_ (\i -> do
+    tanque <- getTanqueAleatorio
+    putStrLn $ "  Execução " ++ show i ++ ": " ++ nome tanque
+    ) [1..5]
+  putStrLn "Resultado: PASSOU"
+  putStrLn ""
+  
+  putStrLn "Teste 6: Testar serialização JSON"
+  tanqueJSON <- getTanqueAleatorio
+  putStrLn $ "JSON gerado: " ++ show (encode tanqueJSON)
+  putStrLn "Resultado: PASSOU"
+  putStrLn ""
+
+  
+  putStrLn "=== TESTES CONCLUÍDOS ==="
