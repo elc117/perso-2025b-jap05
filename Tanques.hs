dc84034c70405abb684fdcdaa3e32e3790711cb7
jap05 <159470981+jap05@users.noreply.github.com>
2025-10-06 00:00:50 -0300

    Add files via upload

---
+++ Tanques.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+import Web.Scotty
+import Data.Aeson (ToJSON)
+import GHC.Generics (Generic)
+import System.Random (randomRIO)
+import Control.Monad.IO.Class (liftIO)
+
+data Tanque = Tanque
+  { nome    :: String
+  , nacao   :: String
+  , calibre :: String
+  } deriving (Show, Generic)
+
+instance ToJSON Tanque
+
+baseDeTanques :: [Tanque]
+baseDeTanques =
+  [ Tanque "Tiger I" "Alemanha" "88mm"
+  , Tanque "T-34" "União Soviética" "76.2mm"
+  , Tanque "M4 Sherman" "Estados Unidos" "75mm"
+  , Tanque "Churchill" "Reino Unido" "57mm"
+  , Tanque "Panzer IV" "Alemanha" "75mm"
+  , Tanque "IS-2" "União Soviética" "122mm"
+  , Tanque "Panther A" "Alemanha" "88mm"
+  , Tanque "SU-100" "União Soviética" "100mm"
+  ]
+
+getElementoAleatorio :: [a] -> IO a
+getElementoAleatorio xs = do
+  indice <- randomRIO (0, length xs - 1)
+  return $ xs !! indice
+
+getTanqueAleatorio :: IO Tanque
+getTanqueAleatorio = getElementoAleatorio baseDeTanques
+
+main :: IO ()
+main = scotty 3000 $ do
+  get "/tanque" $ do
+    tanque <- liftIO getTanqueAleatorio
+    json tanque
